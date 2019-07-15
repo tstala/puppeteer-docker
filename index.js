e388a0a94483cfbb9b57bb10c5a319cea9ae3dd0
Tomasz Stala <tomasz.stala@op.pl>
2019-07-15 13:04:28 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+(async() => {
+  try {
+        const browser = await puppeteer.launch({args: ['--no-sandbox']});
+        const page = await browser.newPage();
+        await page.goto('https://www.google.com');
+        let document = await page.evaluate(() => document.documentElement.outerHTML);
+        console.log(document);
+        await browser.close();
+  } catch(err) {
+        console.log(err);
+  }
+})();
